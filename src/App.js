584d2ec2a079a3a6c69ac8341fcab889f7024da9
ThinHubbins <fredrickchinweze30@gmail.com>
2025-12-01 17:26:36 +0100

    Added a login route and updated the 404error page with better UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import Home from './routes/Home';
 import About from './routes/About';
 import NoPage from './routes/NoPage';
+import Blog from './routes/Blog';
+import Shop from './routes/Shop';
+import Contact from './routes/Contact';
+import Team from './routes/Team';
+import Faq from './routes/Faq';
 
 function App() {
   return (
@@ -13,11 +18,16 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='*' element={<NoPage />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/team' element={<Team />} />
+          <Route path='/faq' element={<Faq />} />
         </Routes>
       </BrowserRouter>
 
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/routes/NoPage.jsx
+++ src/routes/NoPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import img404 from './404img/404error.png'
+import './NoPage.css'
+
 
 const NoPage = () => {
   return (
     <div>
-      page not found
+      <div className='no-page'>
+      <div className='row m-0 mt-5'>
+        <div className='col-md-12 text-center'>
+          <div className='box'>
+            <img src={img404} alt="404 Error" className='img-fluid' style={{height: '200px'}}/>
+
+            <h1 className='text-danger'>- 404 -</h1>
+            <h3 className='text-secondary'>Oops! Page Not Found</h3>
+            <div className='btn btn-danger mt-3'><a href='/' className='text-white text-decoration-none'>Go to Home</a></div>    
+          </div>
+        </div></div>
+      </div>
     </div>
   )
 }
